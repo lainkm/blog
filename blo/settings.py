1079dddec685982f60ebb49e92be8742e7a72dd7
lainkm <ytwanglainkm@gmail.com>
2018-03-12 11:33:13 +0800

    v

--- blo/settings.py
+++ blo/settings.py
@@ -12,11 +12,11 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 from decouple import Csv, config
-
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+sys.path.insert(0, os.path.join(BASE_DIR, 'extra_apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
